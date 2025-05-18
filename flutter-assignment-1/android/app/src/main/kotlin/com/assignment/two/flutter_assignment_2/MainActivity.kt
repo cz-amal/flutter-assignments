b47c761db80ea0352387f7185fdbb88407687783
Amal c z <amaladhil020@gmail.com>
2025-05-18 21:58:37 +0530

    assignment 1 is now responsive

---
+++ flutter-assignment-1/android/app/src/main/kotlin/com/assignment/two/flutter_assignment_2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.assignment.two.flutter_assignment_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- flutter-assignment-1/lib/pages/test_page.dart
+++ flutter-assignment-1/lib/pages/test_page.dart
@@ -10,91 +10,188 @@ class TestPage extends StatefulWidget {
 class _TestPageState extends State<TestPage> {
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    print(screenWidth);
     return Scaffold(
-      body: Column(
-        children: [
-          Image.asset(
-            "lib/assets/images/hotel.jpg",
-            height: 291,
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          const Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+        body: screenWidth <= 500
+            ? Column(
                 children: [
-                  Text(
-                    "Hotel Park Residency",
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  Image.asset(
+                    "lib/assets/images/hotel.jpg",
+                    height: 291,
                   ),
-                  Text(
-                    "Thrissur,Kerala",
-                    style: TextStyle(fontSize: 14, color: Colors.grey),
+                  const SizedBox(
+                    height: 20,
                   ),
-                ],
-              ),
-              SizedBox(
-                width: 90,
-              ),
-              Row(
-                children: [
-                  Text("4.3 "),
-                  Icon(
-                    Icons.star,
-                    size: 16,
-                    color: Colors.orange,
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Hotel Park Residency",
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            "Thrissur,Kerala",
+                            style: TextStyle(fontSize: 14, color: Colors.grey),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        width: 90,
+                      ),
+                      Row(
+                        children: [
+                          Text("4.3 "),
+                          Icon(
+                            Icons.star,
+                            size: 16,
+                            color: Colors.orange,
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Column(
+                        children: [
+                          Icon(
+                            Icons.call,
+                            color: Colors.blue,
+                          ),
+                          Text(
+                            "Phone",
+                            style: TextStyle(color: Colors.blue),
+                          )
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Icon(Icons.route, color: Colors.blue),
+                          Text("Route", style: TextStyle(color: Colors.blue))
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Icon(Icons.share, color: Colors.blue),
+                          Text("Share", style: TextStyle(color: Colors.blue))
+                        ],
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 12, horizontal: 25),
+                    child: Text(
+                        "Situated in Trichr, 1.7 km from Bible Tower, Daan Regency features accommodation with a restaurant, free private parking, a shared lounge and a garden. The property is located 2.1 km from Vadakkunnathan Shiva Shacthram, 2.1 km from Thrissur Pooram and 3.1 km from Thiruvambady Sri Krishna Temple. The accommodation provides a 24-hour front desk, room service and organising tours for guests."
+                        "At the hotel, rooms come with a wardrobe. At Daan Regency the rooms have a desk, a flat-screen TV and a private bathroom."
+                        "An Asian breakfast is available each morning at the accommodation."),
                   )
                 ],
               )
-            ],
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          const Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Column(
+            : Row(
                 children: [
-                  Icon(
-                    Icons.call,
-                    color: Colors.blue,
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(12),
+                      child: Image.asset(
+                        "lib/assets/images/hotel.jpg",
+                        width: 300,
+                        height: 200,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
                   ),
-                  Text(
-                    "Phone",
-                    style: TextStyle(color: Colors.blue),
-                  )
+                  const Expanded(
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 55,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.only(left: 150),
+                              child: Text(
+                                "Hotel Park Residency",
+                                style: TextStyle(
+                                    fontSize: 22, fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            Row(
+                              children: [
+                                Text("4.3 "),
+                                Icon(
+                                  Icons.star,
+                                  size: 16,
+                                  color: Colors.orange,
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                        Text(
+                          "Thrissur,Kerala",
+                          style: TextStyle(fontSize: 14, color: Colors.grey),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(
+                              vertical: 12, horizontal: 25),
+                          child: Text(
+                              "Situated in Trichr, 1.7 km from Bible Tower, Daan Regency features accommodation with a restaurant, free private parking, a shared lounge and a garden. The property is located 2.1 km from Vadakkunnathan Shiva Shacthram, 2.1 km from Thrissur Pooram and 3.1 km from Thiruvambady Sri Krishna Temple. The accommodation provides a 24-hour front desk, room service and organising tours for guests."
+                              "At the hotel, rooms come with a wardrobe. At Daan Regency the rooms have a desk, a flat-screen TV and a private bathroom."
+                              "An Asian breakfast is available each morning at the accommodation."),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Column(
+                              children: [
+                                Icon(
+                                  Icons.call,
+                                  color: Colors.blue,
+                                ),
+                                Text(
+                                  "Phone",
+                                  style: TextStyle(color: Colors.blue),
+                                )
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Icon(Icons.route, color: Colors.blue),
+                                Text("Route",
+                                    style: TextStyle(color: Colors.blue))
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Icon(Icons.share, color: Colors.blue),
+                                Text("Share",
+                                    style: TextStyle(color: Colors.blue))
+                              ],
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
                 ],
-              ),
-              Column(
-                children: [
-                  Icon(Icons.route, color: Colors.blue),
-                  Text("Route", style: TextStyle(color: Colors.blue))
-                ],
-              ),
-              Column(
-                children: [
-                  Icon(Icons.share, color: Colors.blue),
-                  Text("Share", style: TextStyle(color: Colors.blue))
-                ],
-              )
-            ],
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(vertical: 12, horizontal: 25),
-            child: Text(
-                "Situated in Trichr, 1.7 km from Bible Tower, Daan Regency features accommodation with a restaurant, free private parking, a shared lounge and a garden. The property is located 2.1 km from Vadakkunnathan Shiva Shacthram, 2.1 km from Thrissur Pooram and 3.1 km from Thiruvambady Sri Krishna Temple. The accommodation provides a 24-hour front desk, room service and organising tours for guests."
-                "At the hotel, rooms come with a wardrobe. At Daan Regency the rooms have a desk, a flat-screen TV and a private bathroom."
-                "An Asian breakfast is available each morning at the accommodation."),
-          )
-        ],
-      ),
-    );
+              ));
   }
 }

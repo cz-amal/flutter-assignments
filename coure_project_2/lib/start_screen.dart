90f17800c0ba3da8a820a30396b9b330b386fce3
Amal c z <amaladhil020@gmail.com>
2025-05-19 22:02:13 +0530

    added expense tracker

--- coure_project_2/lib/start_screen.dart
+++ coure_project_2/lib/start_screen.dart
@@ -13,20 +13,14 @@ class StartScreen extends StatelessWidget {
         mainAxisSize: MainAxisSize.min,
         children: [
           Image.asset(
-            'assets/images/quiz-logo.png',
+            'lib/assets/images/quiz-logo.png',
             width: 300,
             color: const Color.fromARGB(150, 255, 255, 255),
           ),
-          // Opacity(
-          //   opacity: 0.6,
-          //   child: Image.asset(
-          //     'assets/images/quiz-logo.png',
-          //     width: 300,
-          //   ),
-          // ),
+
           const SizedBox(height: 80),
           Text(
-            'Learn Flutter the fun way!',
+            'Flutter Quiz!',
             style: GoogleFonts.lato(
               color: const Color.fromARGB(255, 237, 223, 252),
               fontSize: 24,
@@ -39,7 +33,7 @@ class StartScreen extends StatelessWidget {
               foregroundColor: Colors.white,
             ),
             icon: const Icon(Icons.arrow_right_alt),
-            label: const Text('Start Quiz'),
+            label: const Text('Start'),
           )
         ],
       ),

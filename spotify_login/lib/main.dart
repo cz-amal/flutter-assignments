b85e7e76f289fe537367fd55a5a0d2c0d3481981
Amal c z <amaladhil020@gmail.com>
2025-06-12 02:17:20 +0530

    finished lyrics page

--- spotify_login/lib/main.dart
+++ spotify_login/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:spotify_login/pages/home_page.dart';
+import 'package:spotify_login/pages/music_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +12,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: MusicPage());
   }
 }
